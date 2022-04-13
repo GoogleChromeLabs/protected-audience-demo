5e676fa03c45808a5753bf861f66d9a524b70aef
Kevin K. Lee <kevinkiklee@gmail.com>
2022-04-13 11:04:08 -0400

    Measure performance for ad auction logic

--- ssp/run-auction.js
+++ ssp/run-auction.js
@@ -1,26 +1,32 @@
-console.log('run-auction.js')
 const runAuction = async () => {
   const auctionConfig = {
-    seller: "https://ssp.localhost:3004",
-    decisionLogicUrl: "https://ssp.localhost:3004/decision-logic.js",
-    interestGroupBuyers: ["https://dsp.localhost:3003"],
-    auctionSignals: { key: "value" },
-    sellerSignals: { key: "value" },
+    seller: 'https://localhost:3004',
+    decisionLogicUrl: 'https://localhost:3004/decision-logic.js',
+    interestGroupBuyers: ['https://localhost:3003'],
+    auctionSignals: { key: 'value' },
+    sellerSignals: { key: 'value' },
     sellerTimeout: 100,
     perBuyerSignals: {
-      "https://dsp.localhost:3003": { key: "value" },
+      'https://localhost:3003': { key: 'value' },
     },
     perBuyerTimeouts: {
-      "https://dsp.localhost:3003": 50,
-      // 'https://another-buyer.example': 200,
-      "*": 150,
+      'https://localhost:3003': 50,
+      '*': 150,
     },
   };
 
-  const opaqueUrl = await navigator.runAdAuction(auctionConfig)
-  console.log({ opaqueUrl })
-  document.getElementById('fledge-ad').src = opaqueUrl
-}
+  performance.mark('adAuctionStart');
+  const opaqueUrl = await navigator.runAdAuction(auctionConfig);
+  performance.mark('adAuctionEnd');
+  performance.measure('adAuctionDuration', 'adAuctionStart', 'adAuctionEnd');
 
-runAuction()
+  performance.mark('adRenderStart');
+  const iframeEl = document.getElementById('fledge-ad');
+  iframeEl.src = opaqueUrl;
+  iframeEl.onload = () => {
+    performance.mark('adRenderEnd');
+    performance.measure('adRenderDuration', 'adRenderStart', 'adRenderEnd');
+  };
+};
 
+runAuction();
