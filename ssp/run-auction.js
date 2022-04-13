f7bfa56a77356b01036dd4777309e5d1c6313120
Kevin K. Lee <kevinkiklee@gmail.com>
2022-04-13 11:11:41 -0400

    Update run-auction.js

--- ssp/run-auction.js
+++ ssp/run-auction.js
@@ -15,17 +15,23 @@ const runAuction = async () => {
     },
   };
 
+  // Run ad auction
   performance.mark('adAuctionStart');
   const opaqueUrl = await navigator.runAdAuction(auctionConfig);
   performance.mark('adAuctionEnd');
   performance.measure('adAuctionDuration', 'adAuctionStart', 'adAuctionEnd');
 
+  // Render ad
   performance.mark('adRenderStart');
   const iframeEl = document.getElementById('fledge-ad');
   iframeEl.src = opaqueUrl;
   iframeEl.onload = () => {
     performance.mark('adRenderEnd');
     performance.measure('adRenderDuration', 'adRenderStart', 'adRenderEnd');
+    performance.measure('adAuctionAndRenderDuration', 'adAuctionStart', 'adRenderEnd');
+
+    console.log('Marks', performance.getEntriesByType('mark'));
+    console.log('Measurements', performance.getEntriesByType('measure'));
   };
 };
 
