414258712629d48f19c6c554fb643ebaf9655528
kevinkiklee <kevinkiklee@gmail.com>
2022-05-11 16:17:21 -0400

    Refactor the overall codebase

--- sites/dsp/bid.js
+++ sites/dsp/bid.js
@@ -13,63 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-function getAdBids(ads, properties) {
-  const { isControversial, windowInnerHeight, remainingAdBudget, favoriteColor } = properties;
-
-  return ads.map((ad) => {
-    const {
-      renderUrl,
-      metadata: { adName, allowControversial, productColor },
-    } = ad;
-
-    // Initial bid
-    let bid = 0;
-
-    if (productColor === favoriteColor) {
-      bid += 1;
-    }
-
-    if (isControversial && !allowControversial) {
-      bid = 0;
-    }
-
-    if (remainingAdBudget[adName] <= 0) {
-      bid = 0;
-    }
-
-    return {
-      adName,
-      bid,
-      renderUrl,
-    };
-  });
-}
-
 function generateBid(interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals, browserSignals) {
-  const { isControversial } = auctionSignals;
-  const { windowInnerHeight } = perBuyerSignals;
-  // const { remainingAdBudget } = trustedBiddingSignals;
-  const {
-    ads,
-    userBiddingSignals: { favoriteColor },
-  } = interestGroup;
-
-  const adBids = getAdBids(ads, {
-    isControversial,
-    windowInnerHeight,
-    remainingAdBudget: 10,
-    favoriteColor,
-  });
-
-  const [{ adName, bid, renderUrl }] = adBids.sort((a, b) => b.bid - a.bid);
+  const [testAd] = interestGroup.ads
 
   return {
+    bid: 1, // Arbitrary bid value
     ad: {
-      adName,
+      adName: testAd.metadata.adName,
     },
-    bid,
-    render: renderUrl,
+    render: testAd.renderUrl,
   };
 }
 
